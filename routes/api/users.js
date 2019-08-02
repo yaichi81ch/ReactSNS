fb4bf90ffc3c353d6226ffc749d048ebda1a103d
yaichi81ch <yaich1.8ichi@gmail.com>
2019-08-02 14:12:48 +0900

    authorization WIP

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check'); // check the documentation
 
 const User = require('../../models/User');
@@ -50,7 +53,21 @@ async (req, res) => {
 
       await user.save();
       // Return jsonwebtoken
-      res.send('Users route');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
 
     } catch(err) {
       console.error(err.message);
