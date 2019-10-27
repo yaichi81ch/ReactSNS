a733ac806e48294ef51b1c4bbe14c92534b50c55
yaichi81ch <yaich1.8ichi@gmail.com>
2019-10-27 17:16:22 +0900

    Add Experience & Education done

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  USER_LOADED
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 
 import { setAlert } from './alert';
@@ -74,7 +76,7 @@ export const login = (email, password) => async dispatch => {
   const body = JSON.stringify({ email, password });
 
   try {
-    const res = await axios.post('/api/auth', body, config);
+    const res = await axios.post('./api/auth', body, config);
 
     dispatch({
       type: LOGIN_SUCCESS,
@@ -93,4 +95,10 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
 };
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -27,7 +28,7 @@ export default function(state = initialState, action) {
       };
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-      localStorage.getItem('token', payload.token);
+      localStorage.setItem('token', payload.token);
       return {
         ...state,
         ...payload,
@@ -37,6 +38,7 @@ export default function(state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
